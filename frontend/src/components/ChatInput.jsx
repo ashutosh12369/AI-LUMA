c5374e14b1e8e11eb1923c16f50a386fa457c115
ashutosh12369 <ashutoshsahu0563@gmail.com>
2026-07-14 14:07:14 +0530

    Fix mobile ChatInput agent pills by making them horizontally scrollable instead of wrapping

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -300,7 +300,7 @@ catch(error){
       <div className="flex flex-col gap-2 bg-white/[0.03] border border-white/[0.07] rounded-2xl px-4 pt-3.5 pb-3">
 
 
-    <div className="flex w-full md:w-[80%] gap-2 pr-2 flex-wrap">
+    <div className="flex w-full gap-2 pr-2 overflow-x-auto [scrollbar-width:none] [&::-webkit-scrollbar]:hidden pb-1">
 
     <button
       onClick={() => setIsAutonomous(!isAutonomous)}
